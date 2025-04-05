090276d54d104a7329f6509054057dd7d5d86f35
asheef <asheefkb@entoo.com>
2025-04-05 17:41:31 +0530

    First Push for Microservice Architecture Project

---
+++ Common-Model-Ms/src/main/java/com/asheef/common_model_ms/CommonModelMsApplication.java
@@ -0,0 +1,13 @@
+package com.asheef.common_model_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonModelMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonModelMsApplication.class, args);
+	}
+
+}
