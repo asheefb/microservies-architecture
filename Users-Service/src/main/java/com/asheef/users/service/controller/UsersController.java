bcd861a7e01706b56704836231fddc7ebce31899
asheef <asheefkb@entoo.com>
2025-04-12 16:15:10 +0530

    updated controller

--- Users-Service/src/main/java/com/asheef/users/service/controller/UsersController.java
+++ Users-Service/src/main/java/com/asheef/users/service/controller/UsersController.java
@@ -1,9 +1,7 @@
 package com.asheef.users.service.controller;
 
 import com.asheef.common_model_mdb.model.utils.ResponseDTO;
-import com.asheef.users.service.dto.CityStateLocationDto;
-import com.asheef.users.service.dto.UsersDto;
-import com.asheef.users.service.dto.UsersUpdateDto;
+import com.asheef.users.service.dto.*;
 import com.asheef.users.service.service.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -33,15 +31,18 @@ public class UsersController {
     }
 
     @PutMapping("update")
-    public ResponseEntity<ResponseDTO> updateUser(UsersUpdateDto usersUpdateDto){
+    public ResponseEntity<ResponseDTO> updateUser(@RequestBody UsersUpdateDto usersUpdateDto){
         return usersService.updateUser(usersUpdateDto);
     }
 
     @PostMapping("add/additional-details")
-    public ResponseEntity<ResponseDTO> addAddtionalDetails(){
-
+    public ResponseEntity<ResponseDTO> addAdditionalDetails(@RequestBody AdditionalDetailsDto additionalDetailsDto){
+        return usersService.addAdditionalDetails(additionalDetailsDto);
     }
 
-
+    @PutMapping("update/additional-details")
+    public ResponseEntity<ResponseDTO> updateAdditionalDetails(@RequestBody AdditionDetailsUpdateDto additionDetailsUpdateDto){
+        return usersService.updateAdditionalDetails(additionDetailsUpdateDto);
+    }
 
 }
